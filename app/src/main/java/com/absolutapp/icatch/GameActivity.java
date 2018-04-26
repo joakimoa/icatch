8fcd7eaa76aa0c96b6d957acc55612d3efa623aa
Patrik Kron <patrik.kron@gmail.com>
2018-04-26 11:24:10 +0200

    Inserted an arrow and added two methods in order to rotate it or change its color.

--- app/src/main/java/com/absolutapp/icatch/GameActivity.java
+++ app/src/main/java/com/absolutapp/icatch/GameActivity.java
@@ -1,15 +1,20 @@
 package com.absolutapp.icatch;
 
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.support.v4.graphics.ColorUtils;
 
 public class GameActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ImageView compass;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,9 +27,12 @@ public class GameActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    rotateArrow(90);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    colorArrow(60);
+
                     return true;
             }
             return false;
@@ -35,10 +43,26 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        compass = (ImageView) findViewById(R.id.compassImageView);
+
     }
 
+
+    protected void rotateArrow(float deg){
+        compass.setRotation(compass.getRotation() + deg);
+
+    }
+
+    /**Sets cclor from red = 0 to green = 100*/
+    protected void colorArrow(float colorGradient){
+        float hue = colorGradient/100*120;
+        int col = ColorUtils.HSLToColor(new float[]{colorGradient,1,0.5f});
+        compass.setColorFilter(col);
+    //compass.setColorFilter();
+    }
 }
+
+
