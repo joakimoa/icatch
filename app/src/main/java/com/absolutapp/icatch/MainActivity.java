0e839bd7f0ad5e8298f9720ddce40d247bf41d9c
Otto <ottosornas@gmail.com>
2018-05-08 14:05:06 +0200

    added a functioning slideshow for a how-to-play

--- app/src/main/java/com/absolutapp/icatch/MainActivity.java
+++ app/src/main/java/com/absolutapp/icatch/MainActivity.java
@@ -3,6 +3,7 @@ package com.absolutapp.icatch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +22,13 @@ public class MainActivity extends AppCompatActivity {
         stopService(new Intent(MainActivity.this, MusicService.class));
         super.onDestroy();
     }
+
+    //reruns the how-to-play
+    public void loadSlides(View view)
+    {
+        new PreferenceManager(this).clearPreference();
+        startActivity(new Intent(this, InstructionsActivity.class));
+        finish();
+    }
 }
+
---
+++ app/src/main/java/com/absolutapp/icatch/PreferenceManager.java
@@ -0,0 +1,68 @@
+package com.absolutapp.icatch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * This class keeps track of if the user has opened the app before.
+ * Only shows the slide-show if it's the first time the app is opened (won't be true for us because of testing purposes)
+ * Created by otto on 2018-05-08.
+ */
+
+public class PreferenceManager {
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+
+    public PreferenceManager(Context context)
+    {
+        this.context = context;
+        getSharedPreference();
+    }
+
+    /**
+     * Gets the shared preferences
+     */
+    private void getSharedPreference()
+    {
+        sharedPreferences = context.getSharedPreferences(context.getString(R.string.my_preference), Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Writes to the shared preferences, marking the app has been opened
+     */
+    public void writePreference()
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(context.getString(R.string.my_preference_key), "INIT_OK");
+        editor.commit();
+    }
+
+    /**
+     * Checks if the app has been opened before and changes the preferences accordinlgy
+     * @return
+     */
+    public boolean checkPreference()
+    {
+        boolean status = false;
+
+        if(sharedPreferences.getString(context.getString(R.string.my_preference_key), "null").equals("null"))
+        {
+            status = false;
+        }
+        else
+        {
+            status = false; //set this to true if you want the class to work
+        }
+        return  status;
+    }
+
+    /**
+     * Clears the preferences if the user wants to see the slideshow again
+     */
+    public void clearPreference()
+    {
+        sharedPreferences.edit().clear().commit();
+    }
+
+}
